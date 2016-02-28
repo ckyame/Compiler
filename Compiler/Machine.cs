29cebfe20809eca5ef8e9ad329320a947eafe063
Chris <Christopher.kyame@laverne.edu>
2016-02-28 13:43:43 -0800

    init

---
+++ Compiler/Machine.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compiler
+{
+    public class Machine
+    {
+        public delegate void StateProcessed(string state);
+        public static StateProcessed OnStateProcessed;
+
+        public delegate void StateProcessedFailed(string error);
+        public static StateProcessedFailed OnStateProcessedFailed;
+
+        public delegate void ParseSuccessful();
+        public static ParseSuccessful OnParseSuccessful;
+
+        public S_PROGRAM HEADER;
+        public S_VAR     LOADEDMEMORY;
+        public S_STAT    LOGICBLOCK;
+        public S_BEGIN   LOGICBEGIN;
+        public S_END     LOGICEND;
+
+        public StringBuilder CPP;
+
+        public Machine()
+        {
+            HEADER       = new S_PROGRAM();
+            LOADEDMEMORY = new S_VAR();
+            LOGICBEGIN   = new S_BEGIN();
+            LOGICBLOCK   = new S_STAT();
+            LOGICEND     = new S_END();
+
+            CPP          = new StringBuilder();
+        }
+
+        public void Parse(string code)
+        {
+            List<string> chunked = code.Split(new[] {"\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> processed = new List<string>();
+            chunked.ForEach(chunk => processed.Add(chunk.TrimStart()));
+            List<string> headerChunks = processed[0].Split(' ').Where(chunk => chunk != "").ToList();
+            string varExpectation = processed[1].Trim();
+            string declarations = processed[2].Trim();
+            int beginIndex = processed.IndexOf("BEGIN");
+            int endIndex = processed.IndexOf("END");
+
+            if (ProcessHeader(headerChunks))
+            {
+                if (LOADEDMEMORY.Process(varExpectation, CPP) && LOADEDMEMORY.Process(declarations, CPP))
+                {
+                    if (LOGICBEGIN.Process(processed[beginIndex], CPP) || LOGICEND.Process(processed[endIndex], CPP))
+                    {
+                        for (int i = beginIndex + 1; i < endIndex; i++)
+                        {
+                            if (LOGICBLOCK.Process(processed[i], CPP)) {}
+                            else
+                            {
+                                CPP.Clear();
+                                Machine.OnStateProcessedFailed("Error in logic block.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        CPP.Clear();
+                        Machine.OnStateProcessedFailed("Invalid Logic Control.");
+                    }
+                }
+                else
+                {
+                    CPP.Clear();
+                    Machine.OnStateProcessedFailed("Error near declarations.");
+                }
+            }
+            else
+            {
+                CPP.Clear();
+                Machine.OnStateProcessedFailed("Header of code invalid.");
+            }
+            CPP.Append("}");
+            Machine.OnParseSuccessful();
+        }
+
+        public bool ProcessHeader(List<string> headerChunks)
+        {
+            foreach (string chunk in headerChunks)
+            {
+                bool passed = HEADER.Process(chunk, CPP);
+                if (!passed)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Compiler/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Compiler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Machine Machine = new Machine();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Machine.OnStateProcessed += UpdateCompilerWatch;
+            Machine.OnStateProcessedFailed += CatchCompilerError;
+            Machine.OnParseSuccessful += CatchCompilerCompletion;
+        }
+
+        private void CompileButton_Click(object sender, RoutedEventArgs e)
+        {
+            Machine.Parse(Code.Text);
+            CPPCode.Text = Machine.CPP.ToString();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Code.Clear();
+            CompilerWatch.Clear();
+            CPPCode.Clear();
+            Machine.CPP.Clear();
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Create(@"C:\compiled.cpp"))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes(Machine.CPP.ToString());
+                fs.Write(info, 0, info.Length);
+            }
+        }
+
+        private void UpdateCompilerWatch(string state)
+        {
+            CompilerWatch.Text += string.Format("\n{0}", state);
+
+        }
+
+        private void CatchCompilerError(string error)
+        {
+            MessageBox.Show(error, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void CatchCompilerCompletion()
+        {
+            MessageBox.Show("Code fully parsed.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
